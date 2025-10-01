215821bf389717436f4de1e2b1a9291140d69e7d
UTKARSH MOHAN <115492964+UTKARSHMOHAN2003@users.noreply.github.com>
2025-10-01 21:41:37 +0530

    Create Knapsack Problem (DP).py

---
+++ Knapsack Problem (DP).py
@@ -0,0 +1,19 @@
+def knapsack(weights, values, W):
+    n = len(weights)
+    dp = [[0]*(W+1) for _ in range(n+1)]
+
+    for i in range(1, n+1):
+        for w in range(W+1):
+            if weights[i-1] <= w:
+                dp[i][w] = max(values[i-1] + dp[i-1][w-weights[i-1]], dp[i-1][w])
+            else:
+                dp[i][w] = dp[i-1][w]
+
+    return dp[n][W]
+
+
+# Example
+weights = [1, 2, 3]
+values = [60, 100, 120]
+W = 5
+print("Max value:", knapsack(weights, values, W))
